d8ed57425364409184fa1c7d31728f84b8f9c129
magesh-frontend <mageshb4006@gmail.com>
2026-05-06 18:50:18 +0530

    fix: update Vite environment variable

--- src/services/movieApi.js
+++ src/services/movieApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = "https://www.omdbapi.com/";
-const API_KEY = process.env.REACT_APP_OMDB_API_KEY;
+const API_KEY = import.meta.env.VITE_OMDB_API_KEY;
 
 export const searchMovies = async (query, page = 1) => {
   const response = await axios.get(BASE_URL, {
